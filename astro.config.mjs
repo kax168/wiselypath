14af5f2184d4f22e6ee11b628fbe2d016d97ee0c
kx <kx@kxdeMac-mini.local>
2026-02-27 21:11:12 +0800

    Initial Astro site for wiselypath.life

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://wiselypath.com',
+  integrations: [sitemap()],
+  output: 'static',
+  build: {
+    format: 'directory'
+  }
+});
